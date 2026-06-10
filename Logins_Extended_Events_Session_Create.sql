a2406b5459c11436a784dcf7d4ba3e4f1677cb7e
Pablo Alejandro Echeverria Barrios <pabechevb@gmail.com>
2026-06-09 19:30:14 -0600

    Add files via upload

---
+++ Logins_Extended_Events_Session_Create.sql
@@ -0,0 +1,7 @@
+CREATE EVENT SESSION [Logins] ON SERVER 
+ADD EVENT sqlserver.login(
+    ACTION(sqlserver.client_app_name,sqlserver.client_hostname,sqlserver.database_name,sqlserver.username)
+    WHERE ([is_cached]=(0)))
+ADD TARGET package0.event_file(SET filename=N'C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\Log\Logins.xel',max_file_size=(1))
+WITH (MAX_MEMORY=512 KB,EVENT_RETENTION_MODE=ALLOW_SINGLE_EVENT_LOSS,MAX_DISPATCH_LATENCY=30 SECONDS,MAX_EVENT_SIZE=0 KB,MEMORY_PARTITION_MODE=NONE,TRACK_CAUSALITY=OFF,STARTUP_STATE=ON)
+GO
